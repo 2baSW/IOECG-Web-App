35772fd70fc30b1762a7cef109e32d0f3ca982b7
Lucas Zheng <Lucas.zheng42@gmail.com>
2025-03-02 21:20:23 +0100

    Update App.jsx
    Rendre le bouton créer projet accessible

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
+import CreateProjectButton from "./components/CreateProjectButton";
 
 function App() {
-    return (
-        <div className="min-h-screen flex flex-col">
-            <Router>
-                <Header/>
-                <main className="flex-grow container mx-auto px-4">
-                    <Routes>
-                        <Route path="/" element={<HomePage/>}/>
-                        {/* Ajoutez d'autres routes ici */}
-                        <Route path="/mes-projets" element={<div>Page Mes Projets</div>}/>
-                        <Route path="/a-propos" element={<div>Page À propos</div>}/>
-                        <Route path="/support" element={<div>Page Support</div>}/>
-                    </Routes>
-                </main>
-                <Footer/>
-            </Router>
-        </div>
-
-    );
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Router>
+        <Header />
+        <main className="flex-grow container mx-auto px-4">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/mes-projets" element={<div>Page Mes Projets</div>} />
+            <Route path="/a-propos" element={<div>Page À propos</div>} />
+            <Route path="/support" element={<div>Page Support</div>} />
+          </Routes>
+        </main>
+        <Footer style={{ position: "relative" }}>
+          <CreateProjectButton />
+        </Footer>
+      </Router>
+      <CreateProjectButton />
+    
+    </div>
+  );
 }
 
 export default App;
