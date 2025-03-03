35e84ff1ffc68df2743d821ff2d40f44eb0b4d27
Lucas Zheng <Lucas.zheng42@gmail.com>
2025-03-03 08:13:10 +0100

    Update FormCreatingProject.jsx
    ajout des alertes sur les boutons inactifs

--- client/src/components/FormCreatingProject.jsx
+++ client/src/components/FormCreatingProject.jsx
@@ -1,5 +1,15 @@
 const FormCreatingProject = () => 
+{
+    const redirectionCatalogueModele = () =>
     {
+      alert("catalogue des modèles indisponible");
+    }
+
+    const creationProjet = () =>
+    {
+        alert("base de données des projet indisponible");
+    }
+    
     const ArrierePlan = {
       '--form-width': '400px',
       '--form-bg-color': 'white',
@@ -37,7 +47,7 @@ const FormCreatingProject = () =>
             <label htmlFor="fname"className="text-blue-600">Sélectionner dans le catalogue de modèle</label>
             <br></br>
             {/*https://flowbite.com/docs/components/buttons/#icon-buttons*/}
-            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button type="button" onClick={redirectionCatalogueModele} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 14 10">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                 </svg>
@@ -52,7 +62,7 @@ const FormCreatingProject = () =>
                 <button type="submit" className="bg-red-500 text-white p-2 rounded-lg w-32">
                   Annuler
                 </button>
-                <button type="button" className="bg-blue-700 text-white p-2 rounded-lg w-32">
+                <button type="button" onClick={creationProjet} className="bg-blue-700 text-white p-2 rounded-lg w-32">
                   Creer
                 </button>
             </div>
