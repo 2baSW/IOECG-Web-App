2aaccd4a3f618b1ab32208f3c06f364f5906391d
2baSW <sows32000@gmail.com>
2025-04-06 18:24:06 +0200

    Ahmed FALL -> Ahmad FALL

--- client/src/pages/SupportPage.jsx
+++ client/src/pages/SupportPage.jsx
@@ -3,7 +3,7 @@ function SupportPage() {
       <div className="p-8">
         <h1 className="text-2xl font-bold mb-4">Support</h1>
         <p>
-          Pour toute question ou demande d’assistance, contactez Mr Ahmed FALL via <a href="mailto:Ahmed.fall@ird.fr" className="text-blue-500 underline">Ahmed.fall@ird.fr</a>.
+          Pour toute question ou demande d’assistance, contactez Mr Ahmad FALL via <a href="mailto:Ahmad.fall@ird.fr" className="text-blue-500 underline">Ahmad.fall@ird.fr</a>.
         </p>
       </div>
     );
