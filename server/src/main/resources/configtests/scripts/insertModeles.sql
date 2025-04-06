8128adc0a7671d6b8ef4488d9522d2cef43d88f1
2baSW <sows32000@gmail.com>
2025-04-06 14:44:25 +0200

    insertion des scripts dans resources/scripts

---
+++ server/src/main/resources/configtests/scripts/insertModeles.sql
@@ -0,0 +1,8 @@
+INSERT INTO modele (nom, version, description, fichier)
+VALUES 
+  ('Model1', '1.0', 'Classifie les ECGs pathologiques1', '/chemin/Model1_v1.0.onnx'),
+  ('Model1', '1.1', 'Classifie les ECGs pathologiques2', '/chemin/Model1_v1.1.onnx'),
+  ('Model2', '1.0', 'Prédire le fonctionnement', '/chemin/Model2_v1.0.onnx'),
+  ('Model2', '2.0', 'Prédire le fonctionnement', '/chemin/Model2_v2.0.onnx'),
+  ('Model3', '1.0', 'Analyse maladie', '/chemin/Model3_v1.O.onnx');
+
